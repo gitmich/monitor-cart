ea6780776584d4d0bf25f250ff147cdcb4a9cf2e
Michael Chen <michaelrchen@gmail.com>
2023-12-10 06:17:59 +0000

    add direction control to motor.

--- web-control/web-control.py
+++ web-control/web-control.py
@@ -1,5 +1,7 @@
 # app.py
 from flask import Flask, render_template
+import RPi.GPIO as GPIO
+import motor
 
 app = Flask(__name__)
 
@@ -11,11 +13,26 @@ def index():
 def move(direction, action):
     if action == "down":
         print(f"Button {direction} pressed")
+        if direction == "up":
+            motor.forward()
+        elif direction == "down":
+            motor.backward()
+        elif direction == "left":
+            motor.left()
+        elif direction == "right":
+            motor.right()
     elif action == "up":
         print(f"Button {direction} released")
+        print("stop.........")
+        motor.stop()
     return f"{direction} button {action}"
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    try:
+        app.run(host='0.0.0.0', port=5000, debug=True)
+    except Exception as e:
+        GPIO.cleanup()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
 
