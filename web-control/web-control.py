d611efa4d5df13bcd751e8905dd64827985d5576
Michael Chen <michaelrchen@gmail.com>
2023-12-10 13:53:39 +0000

    the feature of streaming the video from pi camera.

--- web-control/web-control.py
+++ web-control/web-control.py
@@ -1,13 +1,53 @@
 # app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 import RPi.GPIO as GPIO
 import motor
+import picamera
+import threading
+import io, time
+
 
 app = Flask(__name__)
+
+frame = None
+lock = threading.Lock()
+
+
+def generate_camera_stream():
+    global frame
+    with picamera.PiCamera() as camera:
+        # 設置相機參數
+        camera.resolution = (640, 480)
+        camera.rotation = 180
+        #camera.framerate = 24
+        time.sleep(2)  # 讓相機預熱
+
+        stream = io.BytesIO()
+        for _ in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
+            with lock:
+                stream.seek(0)
+                frame = stream.read()
+            stream.seek(0)
+            stream.truncate()
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+def stream():
+    global frame
+    while True:
+        with lock:
+            if frame:
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        time.sleep(0.1) 
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(stream(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 @app.route('/move/<direction>/<action>')
 def move(direction, action):
@@ -30,8 +70,12 @@ def move(direction, action):
 
 if __name__ == '__main__':
     try:
-        app.run(host='0.0.0.0', port=5000, debug=True)
+        t = threading.Thread(target=generate_camera_stream)
+        t.start()
+        #app.run(host='0.0.0.0', port=5000, debug=True)
+        app.run(host='0.0.0.0', port=5000)
     except Exception as e:
+        print(e)
         GPIO.cleanup()
     except KeyboardInterrupt:
         GPIO.cleanup()
